5464ad7f3ed178e7b04b9c0643175f44f4ef5163
Jan Bölsche <regular.gonzales@googlemail.com>
2018-11-04 18:20:14 +0100

    Demo renders branch names created by init

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const {client} = require('tre-client')
+const pull = require('pull-stream')
+const collectMutations = require('collect-mutations')
+const MutantArray = require('mutant/array')
+const MutantMap = require('mutant/map')
+const h = require('mutant/html-element')
 
 client( (err, ssb, config) => {
   console.log('tre config', config.tre)
   if (err) return console.error(err)
   ssb.whoami( (err, feed) => {
     if (err) return console.error(err)
-    console.log('pub key', feed.id) 
+    console.log('pub key', feed.id)
+    const arr = MutantArray()
+    document.body.appendChild(
+      h('ol', MutantMap(arr, m => {
+        console.log(m())
+        return h('li', m().value.content.name)
+      }, (a,b) => a===b ))
+    )
+    pull(
+      ssb.revisions.messagesByBranch(config.tre.branches.root),
+      collectMutations(arr)
+    )
   }) 
 })
