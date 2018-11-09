77961f325582003fa58c4a9c846ab170a9c47dc8
Jan Bölsche <regular.gonzales@googlemail.com>
2018-11-09 10:08:25 +0100

    Refactor init.js, create node_modules symlink, merge pkg.tre-init into sbot conf

--- bin/init.js
+++ bin/init.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+const fs = require('fs')
 const crypto = require('crypto')
 const {join} = require('path')
 const createSbot = require('scuttlebot-release/node_modules/scuttlebot')
+const merge = require('lodash.merge')
 
 const pull = require('pull-stream')
 const ssbKeys = require('scuttlebot-release/node_modules/ssb-keys')
@@ -9,6 +11,8 @@ const mkdirp = require('mkdirp')
 
 const path = join(process.cwd(), '.tre')
 mkdirp.sync(path)
+fs.symlinkSync(fs.realpathSync('node_modules'), path + '/node_modules')
+
 const caps = crypto.randomBytes(32).toString('base64')
 const port = Math.floor(50000 + 15000 * Math.random())
 
@@ -26,78 +30,94 @@ const branches = [
   {type: 'folder', name: '.Trash', key: 'trash'},
 ]
 
+const keys = ssbKeys.loadOrCreateSync(join(path, 'secret'))
 const browserKeys = ssbKeys.loadOrCreateSync(join(path, 'browser-keys'))
 
-ssbKeys.loadOrCreate(join(path, 'secret'), (err, keys) => {
-  const ssb = createSbot({
-    keys,
-    path,
-    caps: { shs: caps }
-  })
-
-    ssb.whoami( (err, feed) => {
-      if (err) return console.error(err)
-      console.error('pub key', feed.id)
-      console.error('app key', caps)
-      ssb.publish({type: 'root'}, (err, kv) => {
-        if (err) return console.error(err)
-        const root = kv.key
-        const folders = {root}
-        console.error('root', root) 
-        addBranches(branches, root, 0, err => {
-          if (err) console.error(err)
-          console.log(JSON.stringify({
-            caps: {shs: caps},
-            appKey: caps,
-            port,
-            ws: {port: port + 1},
-            master: [browserKeys.id],
-            /*
-            connections: {
-              incoming: {
-                net: [{ port, scope: "public", transform: "shs" }],
-                ws: [{ port: port + 2, scope: "device", transform: "noauth", host: "localhost" }]
-              },
-              outgoing: {
-                net: [{ transform: "shs" }]
-              }
-            },
-            */
-            budo: {
-              host: 'localhost',
-              port: port + 2
-            },
-            tre: {branches: folders},
-            autofollow: keys.public,
-            autoname: folders.machines
-          }, null, 2))
-          setTimeout( ()=> process.exit(0), 200)
-        })
-
-        function addBranches(children, branch, level, cb) {
-          pull(
-            pull.values(children || []),
-            pull.map( o => ({
-              key: o.key,
-              children: o.children,
-              data: Object.assign({root, branch}, o, {children: undefined, key: undefined})
-            })),
-            pull.asyncMap( ({children, key, data}, cb) => {
-              ssb.publish(data, (err, msg) => {
-                if (err) return cb(err)
-                console.error(Buffer.alloc(level).fill(' ').toString(), msg.value.content.name, msg.key)
-                if (key) folders[key] =  msg.key
-                cb(null, {msg, children})
-              })
-            }),
-            pull.asyncMap( ({msg, children}, cb) => addBranches(children, msg.key, level + 1, cb) ),
-            pull.onEnd(cb)
-          )
-        }
-
-      })
-    })
-
+const ssb = createSbot({
+  keys, path, caps: {
+    shs: caps
+  }
 })
 
+ssb.whoami( (err, feed) => {
+  if (err) return console.error(err)
+  console.error('pub key', feed.id)
+  console.error('app key', caps)
 
+  buildTree(ssb, branches, (err, folders) => {
+    if (err) console.error(err)
+    const config = {
+      caps: {shs: caps},
+      appKey: caps,
+      port,
+      ws: {port: port + 1},
+      master: [browserKeys.id],
+      budo: {
+        host: 'localhost',
+        port: port + 2
+      },
+      tre: {branches: folders},
+      autofollow: keys.public,
+      autoname: folders.machines
+    }
+    mergeFromPackageJson(config)
+    writeConfig(config)
+    setTimeout( ()=> process.exit(0), 200)
+  })
+})
+
+function mergeFromPackageJson(config) {
+  let pkg
+  try {
+    pkg = JSON.parse(fs.readFileSync('package.json'))
+  } catch(err) {
+    console.error('Unable to read package.json:', err.message)
+    return
+  }
+  const c = pkg['tre-init']
+  if (!c) return
+  merge(config, c)
+}
+
+function writeConfig(config) {
+  const file = '.trerc'
+  if (fs.existsSync(file)) {
+    return console.error(`${file} already exists, won't overwrite.`)
+  }
+  fs.writeFileSync(file, JSON.stringify(config, null, 2), 'utf8')
+}
+
+function buildTree(ssb, branches, cb) {
+  ssb.publish({type: 'root'}, (err, kv) => {
+    if (err) return cb(err)
+    const root = kv.key
+    const folders = {root}
+    console.error('root', root) 
+    addBranches(branches, root, 0, err => {
+      if (err) return cb(err)
+      cb(null, folders)
+    })
+
+    function addBranches(children, branch, level, cb) {
+      pull(
+        pull.values(children || []),
+        pull.map( o => ({
+          key: o.key,
+          children: o.children,
+          data: Object.assign({root, branch}, o, {children: undefined, key: undefined})
+        })),
+        pull.asyncMap( ({children, key, data}, cb) => {
+          ssb.publish(data, (err, msg) => {
+            if (err) return cb(err)
+            console.error(Buffer.alloc(level).fill(' ').toString(), msg.value.content.name, msg.key)
+            if (key) folders[key] =  msg.key
+            cb(null, {msg, children})
+          })
+        }),
+        pull.asyncMap( ({msg, children}, cb) => addBranches(children, msg.key, level + 1, cb) ),
+        pull.onEnd(cb)
+      )
+    }
+
+  })
+}
